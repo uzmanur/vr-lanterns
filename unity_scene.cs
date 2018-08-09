9c1c90fbc8dce804d9bfe22bae753744f77cf07c
Uzma Nur <26993374+uzmanur@users.noreply.github.com>
2018-08-09 00:08:32 -0500

    Update unity_scene.cs

--- unity_scene.cs
+++ unity_scene.cs
@@ -21,45 +21,72 @@ public class Lantern : MonoBehaviour {
 		Renderer rendStand = stand.GetComponent<Renderer> ();
 		rendStand.material = Resources.Load("StoneSurface") as Material;
 
-
 		GameObject lantern = Resources.Load("Lantern") as GameObject;
 		lantern = Instantiate (lantern, new Vector3 (0f, ly, 0f), Quaternion.identity);
 		lantern.name = "Lantern";
 
 		GameObject paper = GameObject.Find ("/Lantern/Paper");
-
-		GameObject rim = GameObject.Find ("/Lantern/Rim");
-
-		GameObject wick = GameObject.Find ("/Lantern/Wick");
-
-		GameObject wire1 = GameObject.Find ("/Lantern/Wire1");
-		GameObject wire2 = GameObject.Find ("/Lantern/Wire2");
-		GameObject wire3 = GameObject.Find ("/Lantern/Wire3");
-		GameObject wire4 = GameObject.Find ("/Lantern/Wire4");
-		GameObject[] wires = { wire1, wire2, wire3, wire4 };
-
 		Material paperMat = new Material (Shader.Find ("Standard"));
 		paperMat.color = new Color32 (255, 250, 226, 255);
 		paperMat.SetFloat("_Mode", 3);
 		Renderer rendPaper = paper.GetComponent<Renderer> ();
 		rendPaper.material = paperMat;
 
+		GameObject rim = GameObject.Find ("/Lantern/Rim");
 		Material rimMat = new Material (Shader.Find ("Standard"));
 		rimMat.color = new Color32 (255, 255, 255, 255);
 		Renderer rendRim = rim.GetComponent<Renderer> ();
 		rendRim.material = rimMat;
 
+		GameObject wick = GameObject.Find ("/Lantern/Wick");
 		Material wickMat = new Material (Shader.Find ("Standard"));
 		wickMat.color = new Color32 (178, 95, 0, 255);
 		Renderer rendWick = wick.GetComponent<Renderer> ();
 		rendWick.material = wickMat;
 
+		GameObject wire1 = GameObject.Find ("/Lantern/Wire1");
+		GameObject wire2 = GameObject.Find ("/Lantern/Wire2");
+		GameObject wire3 = GameObject.Find ("/Lantern/Wire3");
+		GameObject wire4 = GameObject.Find ("/Lantern/Wire4");
+		GameObject[] wires = { wire1, wire2, wire3, wire4 };
 		Material wireMat = new Material (Shader.Find ("Standard"));
 		wireMat.color = new Color32 (0, 0, 0, 255);
 		for (int i = 0; i < 4; i++) {
 			Renderer rendWire = wires [i].GetComponent<Renderer> ();
 			rendWire.material = wireMat;
 		}
+
+		ParticleSystem flame = wick.AddComponent<ParticleSystem>() as ParticleSystem;
+		// accessing main module of particle system
+		var fm = flame.main;
+		fm.startSpeed = 1f;
+		fm.startSize = 1.5f;
+		fm.startRotation = (float) Math.PI;
+		// accessing shape module of particle system
+		var fs = flame.shape;
+		fs.angle = 0;
+		fs.radius = 0.25f * (float) Math.Sqrt (2);
+		// accessing size over lifetime module of particle system
+		var fsz = flame.sizeOverLifetime;
+		fsz.enabled = true;
+		AnimationCurve curve = new AnimationCurve();
+		curve.AddKey (0f, 0.5f);
+		curve.AddKey (0.05f, 0.45f);
+		curve.AddKey (0.125f, 0.25f);
+		curve.AddKey (0.20f, 0.05f);
+		curve.AddKey (0.25f, 0f);
+		fsz.size = new ParticleSystem.MinMaxCurve(1.0f, curve);
+		// accessing lights module of particle system
+		var fl = flame.lights;
+		fl.enabled = true;
+		fl.ratio = 1f;
+		fl.light = Resources.Load ("EffectExamples/Shared/Prefabs/ParticlesLight") as Light;
+		fl.useRandomDistribution = false;
+		fl.useParticleColor = false;
+		fl.maxLights = 100;
+		// accessing renderer module of particle system
+		ParticleSystemRenderer rendFlame = flame.GetComponent<ParticleSystemRenderer> ();
+		rendFlame.material = Resources.Load("EffectExamples/FireExplosionEffects/Materials/FlameRoundYellowParticle") as Material;
 
 		// places all lanterns inside a circle with radius of r lanterns
 		int c = 0;
@@ -77,38 +104,6 @@ public class Lantern : MonoBehaviour {
 				}
 			}
 		}
-
-
-
-//		// places all lanterns in square with side length 15
-//		for (int i = 0; i < 2; i++) {
-//			for (int j = 0; j < 112; j++) {
-//				float n = 0f;
-//				int k = j;
-//				if (i == 0) { 
-//					n = 1f;
-//					k = j + 1;
-//				}
-//				if (i == 1) { 
-//					n = -1f; 
-//					k = j + 113;
-//				}
-//				float x = n*(-70+10*((Mathf.Floor(j/15))));
-//				float z = n*(-70+10*((j%15)));
-//				GameObject lClone = Instantiate(lantern, new Vector3 (x, ly, z), Quaternion.identity);
-//				GameObject sClone = Instantiate(stand, new Vector3 (x, sy, z), Quaternion.Euler(-90f, 0f, 0f));
-//				if (n > 0) {
-//					lClone.name = "LanternClone" + k.ToString ("000");
-//					sClone.name = "StandClone" + k.ToString ("000");
-//				}
-//				if (n < 0) {
-//					lClone.name = "LanternClone" + k.ToString ("000");
-//					sClone.name = "StandClone" + k.ToString ("000");
-//				}
-//
-//			}
-//		}
-
 	}
 	
 	// Update is called once per frame
